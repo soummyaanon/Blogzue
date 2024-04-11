14829b7b61eff192eae06b4a1838e35e48d241c3
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-04-11 23:16:41 +0530

    My major Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+
+  return (
+    <>
+    <h1>A blog app with appwrite and vite</h1>
+    </>
+  )
+}
+
+export default App
