91356f5a08f25180dbe44b45b02269a22135500f
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-04-14 22:40:47 +0530

    Refactor component imports and update Header component in index.js and Header.jsx

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,14 @@
-
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
-export { Header, Footer };
+export{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+
+}
