e10d504892ba01f65579833cf674b10d0e3da8f7
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-04-27 16:44:42 +0530

    Add react-social-icons package and update dependencies in package.json and package-lock.json

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../Logo'
+import { SocialIcon } from 'react-social-icons'
 
 function Footer() {
   return (
@@ -10,7 +11,7 @@ function Footer() {
             <div className="w-full p-6 md:w-1/2 lg:w-5/12">
                 <div className="flex h-full flex-col justify-between">
                     <div className="mb-4 inline-flex items-center">
-                        <Logo width="100px" />
+                        <Logo width="200px" />
                     </div>
                     <div>
                         <p className="text-sm">
@@ -20,43 +21,27 @@ function Footer() {
                 </div>
             </div>
             <div className="w-full p-6 md:w-1/2 lg:w-7/12">
-                <div className="h-full">
+                <div className="flex flex-col items-start">
                     <h3 className="tracking-px mb-9 text-xs font-semibold uppercase">
                         Follow Me
                     </h3>
-          <ul>
-    <li className="mb-4">
-        <a
-            href="https://twitter.com/Thesourya2000"
-            className="text-base font-medium text-white-300 hover:text-gray-700"
-        >
-            X
-        </a>
-    </li>
-    <li className="mb-4">
-        <a
-            href="https://github.com/soummyaanon"
-            className="text-base font-medium text-white-300 hover:text-gray-700"
-        >
-            GitHub
-        </a>
-    </li>
-    <li>
-        <a
-            href="https://www.linkedin.com/in/soumyapandaofficial/"
-            className="text-base font-medium text-white-300 hover:text-gray-700"
-        >
-            LinkedIn
-        </a>
-    </li>
-</ul>
+                    <ul className="flex space-x-4">
+                        <li>
+                            <SocialIcon url="https://x.com/Thesourya2000" className="text-base font-medium text-white-300 hover:text-gray-700" />
+                        </li>
+                        <li>
+                            <SocialIcon url="https://github.com/soummyaanon" className="text-base font-medium text-white-300 hover:text-gray-700" />
+                        </li>
+                        <li>
+                            <SocialIcon url="https://www.linkedin.com/in/soumyapandaofficial/" className="text-base font-medium text-white-300 hover:text-gray-700" />
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
-  )
-}
+    );
+};
 
 export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import appwriteService from '../appwrite/config';
 import Container from '../components/container/Container';
 
 
+
 // Import the images
 import technologiesImg from '../assets/tech.png';
 import natureImg from '../assets/nature.png';
@@ -27,20 +28,21 @@ function Home() {
 
   if (posts.length === 0) {
     return (
-<div className="w-full py-8 mt-4 md-6 text-center bg-black">
-    <div className="w-72 mx-auto p-4">
-        <div className="p-2 w-full text-center">
-            <h1 className="text-xl font-bold text-white">
-                "Become a successful blogger."
-            </h1>
-            <p className="mt-4 text-white text-lg font-semibold tracking-wide italic">
-                "Join our community, share your thoughts, and start your blogging journey."
-            </p>
-            <Link to="/signup" className="mt-4 inline-block bg-gray-800 text-white px-6 py-2 rounded font-bold text-lg border-2 border-white hover:bg-gray-700 transition-colors duration-300 ease-in-out transform hover:scale-110">
-                Get Started
-            </Link>
-        </div>
-    </div>
+      <div className="w-full py-8 mt-4 md-6 text-center bg-black">
+      <div className="mx-auto p-4 text-center font-serif">
+          <h1 className="text-2xl font-bold text-white">
+              "Become a successful blogger. Blogging is a great way to express your thoughts, share your knowledge with the world, and make a difference in people's lives. It can also be a profitable business if done right."
+          </h1>
+          <p className="mt-4 text-white text-lg font-semibold tracking-wide italic">
+              "Join our community, share your thoughts, and start your blogging journey. 'The journey of a thousand miles begins with a single step.' - Lao Tzu"
+          </p>
+          <Link to="/signup" className="mt-4 inline-block bg-gray-800 text-white px-6 py-2 rounded font-bold text-lg border-2 border-white hover:bg-gray-700 transition-colors duration-300 ease-in-out transform hover:scale-110">
+              Get Started
+          </Link>
+      </div>
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" viewBox="0 0 2400 800">
+        {<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" viewBox="0 0 2400 800"><defs><linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="sssurf-grad"><stop stop-color="hsl(208, 77%, 50%)" stop-opacity="1" offset="0%"></stop><stop stop-color="hsl(208, 74%, 93%)" stop-opacity="1" offset="100%"></stop></linearGradient></defs><g fill="url(#sssurf-grad)" transform="matrix(1,0,0,1,0,-92.80804443359375)"><path d="M 0 318.204036378355 Q 450 453.6456041116991 600 326.12561021670217 Q 1050 486.6042829652782 1200 305.6160852796144 Q 1650 483.88218801658974 1800 328.6725424189918 Q 2250 511.3924772769642 2400 336.4378925677312 L 2400 800 L 0 800 L 0 315.25554622989887 Z" transform="matrix(1,0,0,1,0,35)" opacity="0.05"></path><path d="M 0 318.204036378355 Q 450 453.6456041116991 600 326.12561021670217 Q 1050 486.6042829652782 1200 305.6160852796144 Q 1650 483.88218801658974 1800 328.6725424189918 Q 2250 511.3924772769642 2400 336.4378925677312 L 2400 800 L 0 800 L 0 315.25554622989887 Z" transform="matrix(1,0,0,1,0,70)" opacity="0.21"></path><path d="M 0 318.204036378355 Q 450 453.6456041116991 600 326.12561021670217 Q 1050 486.6042829652782 1200 305.6160852796144 Q 1650 483.88218801658974 1800 328.6725424189918 Q 2250 511.3924772769642 2400 336.4378925677312 L 2400 800 L 0 800 L 0 315.25554622989887 Z" transform="matrix(1,0,0,1,0,105)" opacity="0.37"></path><path d="M 0 318.204036378355 Q 450 453.6456041116991 600 326.12561021670217 Q 1050 486.6042829652782 1200 305.6160852796144 Q 1650 483.88218801658974 1800 328.6725424189918 Q 2250 511.3924772769642 2400 336.4378925677312 L 2400 800 L 0 800 L 0 315.25554622989887 Z" transform="matrix(1,0,0,1,0,140)" opacity="0.53"></path><path d="M 0 318.204036378355 Q 450 453.6456041116991 600 326.12561021670217 Q 1050 486.6042829652782 1200 305.6160852796144 Q 1650 483.88218801658974 1800 328.6725424189918 Q 2250 511.3924772769642 2400 336.4378925677312 L 2400 800 L 0 800 L 0 315.25554622989887 Z" transform="matrix(1,0,0,1,0,175)" opacity="0.68"></path><path d="M 0 318.204036378355 Q 450 453.6456041116991 600 326.12561021670217 Q 1050 486.6042829652782 1200 305.6160852796144 Q 1650 483.88218801658974 1800 328.6725424189918 Q 2250 511.3924772769642 2400 336.4378925677312 L 2400 800 L 0 800 L 0 315.25554622989887 Z" transform="matrix(1,0,0,1,0,210)" opacity="0.84"></path><path d="M 0 318.204036378355 Q 450 453.6456041116991 600 326.12561021670217 Q 1050 486.6042829652782 1200 305.6160852796144 Q 1650 483.88218801658974 1800 328.6725424189918 Q 2250 511.3924772769642 2400 336.4378925677312 L 2400 800 L 0 800 L 0 315.25554622989887 Z" transform="matrix(1,0,0,1,0,245)" opacity="1"></path></g></svg>}
+    </svg>
 </div>
     );
   }
