20df178a7527d410a19a555240db3ee3ddc86067
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-04-30 12:53:36 +0530

    Update body background color to gray in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
@@ -23,19 +22,17 @@ function App() {
     .finally(() => setLoading(false))
   }, [dispatch])
   
-  return !loading ? (
-    <>
-<div className='min-h-screen flex flex-col justify-between bg-gray-900 text-gray-200'>
+return !loading ? (
+  <div style={{ backdropFilter: 'blur(10px)' }} className='min-h-screen flex flex-col justify-between bg-gray-900 text-gray-200 bg-opacity-50'>
   <div className='w-full block'>
-    <Header className='text-2xl font-bold p-4  transition-colors duration-200' />
-    <main className='px-4 py-8'>
-      <Outlet />
-    </main>
-    <Footer className='text-sm p-4 bg-gray-800 hover:text-white transition-colors duration-200' />
+      <Header className='text-2xl font-bold p-4  transition-colors duration-200' />
+      <main className='px-4 py-8'>
+        <Outlet />
+      </main>
+      <Footer className='text-sm p-4 bg-gray-800 hover:text-white transition-colors duration-200' />
+    </div>
   </div>
-</div>
-    </>
-  ) : null
+) : null
 }
 
 export default App
