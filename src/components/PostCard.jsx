b72975f4eef4704b898d349695ac269d61b075c4
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-05-01 14:42:39 +0530

    Update Login component background color to gray

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,17 +5,14 @@ import {Link} from 'react-router-dom'
 function PostCard({$id, title, featuredImage }) {
   
   return (
-  <Link to={`/post/${$id}`}>
-  <div className='w-full h-64 bg-gray-600 rounded-xl p-4 shadow-lg transform hover:scale-105 transition duration-300 mb-4'>
-    <div className='w-full h-full justify-center mb-4'>
-      <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-      className='w-full h-full object-cover rounded-xl' />
-    </div>
-    <h2
-      className='text-xl font-bold text-white' 
-    >{title}</h2>
-  </div>
-</Link>
+    <Link to={`/post/${$id}`}>
+      <div className='w-full h-64 bg-gray-700 rounded-xl p-4 shadow-lg transform hover:scale-105 transition duration-300 mb-4'>
+        <div className='w-full h-full justify-center mb-4'>
+          <img src={appwriteService.getFilePreview(featuredImage)} alt={title} className='w-full h-full object-cover rounded-xl' />
+        </div>
+        <h2 className='text-xl font-bold text-white'>{title}</h2>
+      </div>
+    </Link>
   )
 }
 export default PostCard
