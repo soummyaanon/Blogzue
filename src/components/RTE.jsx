ed51ca8916efcfc23ceedfeb151704c3c90da85a
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-05-06 00:08:00 +0530

    chore: Update RTE component to track character count and disable editing after 255 characters

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Editor } from '@tinymce/tinymce-react';
 import { Controller } from 'react-hook-form';
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
-  const [wordCount, setWordCount] = useState(0);
+  const [charCount, setCharCount] = useState(0);
+  const [editor, setEditor] = useState(null);
+
+  useEffect(() => {
+    if (editor) {
+      editor.on('keydown', function (e) {
+        if (charCount >= 255) {
+          e.preventDefault();
+          e.stopPropagation();
+          return false;
+        }
+      });
+    }
+  }, [editor, charCount]);
 
   const handleEditorChange = (content) => {
-    const words = content.trim().split(/\s+/).length;
-    setWordCount(words);
+    const chars = content.length;
+    setCharCount(chars);
   }
 
   return (
@@ -51,6 +64,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
               content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
             }}
+            onInit={(event, editor) => setEditor(editor)}
             onEditorChange={(content) => {
               handleEditorChange(content);
               onChange(content);
@@ -59,7 +73,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         )}
       />
 
-      <p className='text-sm text-red-500'>{wordCount > 30 ? 'Only 30 words are allowed' : null}</p>
+      <p className='text-sm text-red-500'>{charCount > 255 ? 'Only 255 characters are allowed' : null}</p>
     </div>
   )
 }
