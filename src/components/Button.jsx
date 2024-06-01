ecb762e0c11b672c515789b24fec7b487b2a9429
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-06-01 23:44:19 +0530

    chore: Update Button component to increase padding on y-axis

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,7 @@ export default function Button({
     ...props
 }) {
     return (
-        <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
+        <button className={`px-4 py-4 rounded-lg ${bgColor} ${textColor} ${className}`} {...props}>
             {children}
         </button>
     );
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -5,6 +5,6 @@ const conf = {
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
-// there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
+
 
 export default conf
