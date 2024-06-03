fabd148a6472b2d87a42f5b52e1b8add3345dc78
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-06-03 11:46:00 +0530

    chore: Add 3D rotation effect to PostCard component on hover

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -21,17 +21,18 @@ function PostCard({ $id, title, featuredImage, catagory }) {
 
   return (
     <Link to={`/post/${$id}`} className='flex flex-wrap justify-center'>
-<motion.div
-  className='flex-auto w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 h-64 rounded-xl p-6 shadow-lg mb-8  mx-8 flex flex-col justify-between transition-transform duration-300'
-  style={{
-    transform: `perspective(600px) rotateX(${transform.rotateX}deg) rotateY(${transform.rotateY}deg) translateX(${transform.rotateY * 0.5}px) translateY(${transform.rotateX * 0.5}px)`,
-    boxShadow: `${-transform.rotateY * 2}px ${transform.rotateX * 2}px 20px rgba(255, 255, 255, 0.6)`, // White shadow
-    background: `hsl(0, 0%, ${transform.rotateY}%)`
-  }}
-  onMouseMove={handleMouseMove}
-  onMouseLeave={handleMouseLeave}
-  whileHover={{ scale: 1.1 }}
->
+      <motion.div
+        className='flex-auto w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 h-64 rounded-xl p-6 shadow-lg mb-8  mx-8 flex flex-col justify-between transition-transform duration-300'
+        style={{
+          transform: `perspective(600px) rotateX(${transform.rotateX}deg) rotateY(${transform.rotateY}deg) translateX(${transform.rotateY * 0.5}px) translateY(${transform.rotateX * 0.5}px)`,
+          boxShadow: `${-transform.rotateY * 2}px ${transform.rotateX * 2}px 20px rgba(255, 255, 255, 0.6)`, // White shadow
+          background: `hsl(0, 0%, ${transform.rotateY}%)`
+        }}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        whileHover={{ scale: 1.3 }} // Increase scale to 1.3 on hover
+        transition={{ type: 'spring', stiffness: 300, damping: 20 }} // Add this line to make the animation smoother
+      >
         <div className='w-full h-2/3 mb-4'>
           <img
             src={appwriteService.getFilePreview(featuredImage)}
