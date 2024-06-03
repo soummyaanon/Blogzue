973c556031f50084714f59c7350706162432f1c9
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-06-03 11:51:34 +0530

    chore: Update AllPosts component to display posts in responsive grid layout

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -10,17 +10,17 @@ function AllPosts() {
             setPosts(posts.documents)
         }
     })
-  return (
+    return (
     <div className='w-full py-8'>
         <Container>
             <div className='flex flex-wrap'>
                 {posts.map((post) => (
-                    <div key={post.$id} className='p-2 w-1/4'>
+                    <div key={post.$id} className='p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5'>
                         <PostCard {...post} />
                     </div>
                 ))}
             </div>
-            </Container>
+        </Container>
     </div>
   )
 }
