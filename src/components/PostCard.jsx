77445bb70ccc19527dde2b5ee7f4fb15c504ec42
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-06-03 23:47:58 +0530

    Refactor PostCard component to improve hover effect and optimize code

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -21,28 +21,27 @@ function PostCard({ $id, title, featuredImage, catagory }) {
 
   return (
     <Link to={`/post/${$id}`} className='flex flex-wrap justify-center'>
-      <motion.div
-        className='flex-auto w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 h-64 rounded-xl p-6 shadow-lg mb-8  mx-8 flex flex-col justify-between transition-transform duration-300'
-        style={{
-          transform: `perspective(600px) rotateX(${transform.rotateX}deg) rotateY(${transform.rotateY}deg) translateX(${transform.rotateY * 0.5}px) translateY(${transform.rotateX * 0.5}px)`,
-          boxShadow: `${-transform.rotateY * 2}px ${transform.rotateX * 2}px 20px rgba(255, 255, 255, 0.6)`, // White shadow
-          background: `hsl(0, 0%, ${transform.rotateY}%)`
-        }}
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseLeave}
-        whileHover={{ scale: 1.3 }} // Increase scale to 1.3 on hover
-        transition={{ type: 'spring', stiffness: 300, damping: 20 }} // Add this line to make the animation smoother
-      >
-        <div className='w-full h-2/3 mb-4'>
-          <img
-            src={appwriteService.getFilePreview(featuredImage)}
-            alt={title}
-            className='w-full h-full object-cover rounded-lg'
-          />
-        </div>
-        <h2 className='text-lg font-bold text-gray-200'>{title}</h2>
-        <p className='text-sm text-gray-300 mt-2'>{catagory}</p>
-      </motion.div>
+<motion.div
+  className='flex-auto w-full md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 h-64 rounded-xl p-6 shadow-lg mb-8  mx-8 flex flex-col justify-between transition-transform duration-300'
+  style={{
+    transform: `perspective(600px) rotateX(${transform.rotateX}deg) rotateY(${transform.rotateY}deg) translateX(${transform.rotateY * 0.5}px) translateY(${transform.rotateX * 0.5}px)`,
+    boxShadow: `${-transform.rotateY * 2}px ${transform.rotateX * 2}px 20px rgba(255, 255, 255, 0.6)`, // White shadow
+    background: `hsl(0, 0%, ${transform.rotateY}%)`
+  }}
+  onMouseMove={handleMouseMove}
+  onMouseLeave={handleMouseLeave}
+  whileHover={{ y: ["0%", "-15%", "0%"], transition: { repeat: Infinity, duration: 0.5 } }} // Increase float effect and make it faster
+>
+  <div className='w-full h-2/3 mb-4'>
+    <img
+      src={appwriteService.getFilePreview(featuredImage)}
+      alt={title}
+      className='w-full h-full object-cover rounded-lg'
+    />
+  </div>
+  <h2 className='text-lg font-bold text-gray-200'>{title}</h2>
+  <p className='text-sm text-gray-300 mt-2'>{catagory}</p>
+</motion.div>
     </Link>
   );
 }
